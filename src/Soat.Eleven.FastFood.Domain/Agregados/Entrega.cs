17735c7beff4e051a1be3ccbd989bb50efdc1199
Andre Luiz dos Anjos <andre.luiz@procfit.com.br>
2025-05-22 20:42:15 -0300

    Removida pasta agregados

--- src/Soat.Eleven.FastFood.Domain/Agregados/Entrega.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Soat.Eleven.FastFood.Domain.Agregados
-{
-    public class Entrega
-    {
-        public Guid Id { get; private set; }
-        public Guid PedidoId { get; private set; }
-        public DateTime DataRetirada { get; private set; }
-    }
-}
