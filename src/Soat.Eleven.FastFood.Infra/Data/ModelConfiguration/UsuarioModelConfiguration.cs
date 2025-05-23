4d604341ac29c40cf71b88925729cabc197bea4f
DhyogoSiqueira <9dhyogoamerico@gmail.com>
2025-05-22 21:18:25 -0300

    Configura mapeamento das entidades

---
+++ src/Soat.Eleven.FastFood.Infra/Data/ModelConfiguration/UsuarioModelConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Soat.Eleven.FastFood.Domain.Entidades;
+
+namespace Soat.Eleven.FastFood.Infra.Data.ModelConfiguration;
+
+public class UsuarioModelConfiguration : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder.HasOne(u => u.Perfil)
+               .WithMany(p => p.Usuarios)
+               .HasForeignKey(c => c.PerfilId)
+               .OnDelete(DeleteBehavior.NoAction);
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Id).HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(c => c.Nome)
+               .IsRequired();
+
+        builder.Property(c => c.Email)
+               .IsRequired();
+
+        builder.Property(c => c.CriadoEm)
+               .HasDefaultValueSql("NOW()");
+
+        builder.Property(c => c.ModificadoEm)
+               .HasDefaultValueSql("NOW()");
+    }
+}
