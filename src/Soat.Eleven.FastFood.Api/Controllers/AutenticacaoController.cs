ecebe526bdc802d758b9f3ac2b9f8330ad857b91
Adriano Ricardo Felippe Torini <adrianotorini@hotmail.com>
2025-05-23 13:20:14 -0300

    Adiciona controller provisorio de autenticacao

---
+++ src/Soat.Eleven.FastFood.Api/Controllers/AutenticacaoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Soat.Eleven.FastFood.Application.Services.Interfaces;
+
+namespace Soat.Eleven.FastFood.Api.Controllers
+{
+    /// <summary>
+    /// Controller provisorio, deve ser alterado para o controller de autenticação.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly ILogger<AutenticacaoController> _logger;
+        private readonly ITokenAtendimentoService _tokenAtendimentoService;
+
+        public AutenticacaoController(ILogger<AutenticacaoController> logger, ITokenAtendimentoService tokenAtendimentoService)
+        {
+            _logger = logger;
+            _tokenAtendimentoService = tokenAtendimentoService;
+        }
+
+        [HttpPost("Logar")]
+        public async Task<IActionResult> CriarAutenticacao()
+        {
+            
+            return Ok("Autenticacao criada com sucesso.");
+        }
+
+        [HttpPost("RecuperarToken")]
+        public async Task<IActionResult> RecuperarToken()
+        {
+            return Ok("Autenticacao criada com sucesso.");
+        }
+
+        [HttpPost("ValidarToken")]
+        public async Task<IActionResult> ValidarToken()
+        {
+            return Ok("Autenticacao criada com sucesso.");
+        }
+    }
+}
